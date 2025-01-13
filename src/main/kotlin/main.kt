8da19b92b1779dea3dbbc92eedcb899ff8e7f2fa
knokko <knokogator@hotmail.com>
2025-01-13 12:30:23 +0100

    Starting to look like a proof-of-concept

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,16 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.delay
 import java.lang.Thread.sleep
-
-private class TestObject(
-    var x: Int = 1,
-    var text: String = "hello $x"
-) {
-}
 
 fun <T: Any> changeState(state: T, change: T.() -> Unit): T {
     synchronized(state) {
@@ -26,26 +21,28 @@ fun <T: Any> changeState(state: T, change: T.() -> Unit): T {
 }
 
 @Composable
-fun composeArmorTypeOverview(armorTypes: MutableList<ArmorType>) {
+fun composeArmorTypeOverview(armorTypes: MutableList<ArmorType>) = synchronized(armorTypes) {
     println("composeArmorTypeOverview")
+    var state by remember { mutableStateOf(armorTypes, policy = neverEqualPolicy()) }
     Column(verticalArrangement = Arrangement.Bottom) {
-        Column(modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Top) {
-            composeArmorTypesList(armorTypes)
+        Box(Modifier.weight(1f)) {
+            LazyColumn(verticalArrangement = Arrangement.Top) {
+                synchronized(armorTypes) {
+                    items(state) { composeArmorTypeRow(it) }
+                }
+            }
         }
-        composeArmorTypeToolbar(armorTypes)
-    }
-}
-
-@Composable
-fun composeArmorTypeToolbar(armorTypes: MutableList<ArmorType>) {
-    Text("Hello")
-}
-
-@Composable
-fun composeArmorTypesList(armorTypes: MutableList<ArmorType>) {
-    println("composeArmorTypesList")
-    Column {
-        for (armorType in armorTypes) composeArmorTypeRow(armorType)
+        TextButton(onClick = { state = changeState(state) { add(ArmorType("", "", EquipmentSlotType.Body)) } }) {
+            Text("Add armor type")
+        }
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(1000)
+                val wasModified = true
+                if (wasModified) state = state
+                println("Updating list")
+            }
+        }
     }
 }
 
@@ -57,31 +54,14 @@ fun composeArmorTypeRow(armorType: ArmorType) {
     Row {
         TextField(state.key, { state = changeState(state) { key = it } }, label = { Text("key") })
         TextField(state.name, { state = changeState(state) { name = it } }, label = { Text("name") })
-    }
-}
 
-@Composable
-@Preview
-fun App() {
-    println("App()")
-    var test by remember { mutableStateOf(TestObject(), policy = neverEqualPolicy()) }
-
-    Thread {
-        repeat(50) {
-            sleep(1000)
-            test = changeState(test) {
-                x += 1
-                text = "hm $x"
+        LaunchedEffect(Unit) {
+            while (true) {
+                val wasModified = true
+                if (wasModified) state = state
+                delay(1000)
+                println("Updating ${state.name}")
             }
-        }
-    }.start()
-
-    MaterialTheme {
-        Button(
-            onClick = { test = changeState(test) { text = "whoops $x" } },
-            modifier = Modifier.testTag("button")
-        ) {
-            Text(test.text)
         }
     }
 }
@@ -92,6 +72,23 @@ fun main() = application {
         ArmorType("k-shield", "Kite Shield", EquipmentSlotType.Head)
     )
     Window(onCloseRequest = ::exitApplication) {
-        composeArmorTypeOverview(armorTypes)
+        Column {
+            Text("Inventory")
+            Text("Armor types")
+            composeArmorTypeOverview(armorTypes)
+        }
     }
+
+    Thread {
+        sleep(3000)
+        synchronized(armorTypes) {
+            repeat(100_000) {
+                armorTypes.add(ArmorType("a", "b", EquipmentSlotType.Accessory))
+            }
+        }
+        repeat(100) { index ->
+            sleep(1000)
+            armorTypes[index].name = "Overrule $index"
+        }
+    }.start()
 }
